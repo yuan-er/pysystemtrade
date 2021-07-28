97bf0d8ad9e9aca6ffec03fdbb7b884e15c3d2af
qiang-home <qiangwu.cs.nudt@gmail.com>
2021-07-28 09:33:00 +0800

    update: simple full process for etf portfolio

--- examples/introduction/etf_portfolio.py
+++ examples/introduction/etf_portfolio.py
@@ -1,262 +1,127 @@
-"""
-Let's recap:
+# -*- coding:utf-8 -*-
+__author__ = "qiang"
+__license__ = ""
+__version__ = "2021-07-27"
 
-We got some data and created a trading rule
+"""
+验证etf组合是否与backtrader计算结果一致
 """
 from sysdata.sim.csv_futures_sim_data import csvFuturesSimData
-
-data = csvFuturesSimData()
-
 from systems.provided.example.rules import ewmac_forecast_with_defaults as ewmac
-
-"""
-Okay, I wonder how this would work for a number of instruments?
-
-For this we need to build a system
-
-A system is made up of SystemStages - essentially stages in the process, and it
-needs data, and perhaps a configuration
-
-The minimum stage you would have would be Rules - which is where you put
-trading rules
-"""
-
 from systems.forecasting import Rules
-
-"""
-We can create rules in a number of different ways
-
-Note that to make our rule work it needs to have
-"""
-my_rules = Rules(ewmac)
-print(my_rules.trading_rules())
-
-my_rules = Rules(dict(ewmac=ewmac))
-print(my_rules.trading_rules())
-
-from systems.basesystem import System
-
-my_system = System([my_rules], data)
-print(my_system)
-
-print(my_system.rules.get_raw_forecast("159949", "ewmac").tail(5))
-"""
-Define a TradingRule
-"""
-
 from systems.trading_rules import TradingRule
-
-ewmac_8 = TradingRule((ewmac, [], dict(Lfast=8, Lslow=32)))
-ewmac_32 = TradingRule(
-    dict(
-        function=ewmac,
-        other_args=dict(
-            Lfast=32,
-            Lslow=128)))
-my_rules = Rules(dict(ewmac8=ewmac_8, ewmac32=ewmac_32))
-print(my_rules.trading_rules()["ewmac32"])
-
-my_system = System([my_rules], data)
-my_system.rules.get_raw_forecast("EDOLLAR", "ewmac32").tail(5)
-
+from systems.basesystem import System
 from sysdata.config.configdata import Config
-
-my_config = Config()
-
-empty_rules = Rules()
-my_config.trading_rules = dict(ewmac8=ewmac_8, ewmac32=ewmac_32)
-my_system = System([empty_rules], data, my_config)
-my_system.rules.get_raw_forecast("EDOLLAR", "ewmac32").tail(5)
-
 from systems.forecast_scale_cap import ForecastScaleCap
-
-# we can estimate these ourselves
-my_config.instruments = ["US10", "EDOLLAR", "CORN", "SP500"]
-my_config.use_forecast_scale_estimates = True
-
-fcs = ForecastScaleCap()
-my_system = System([fcs, my_rules], data, my_config)
-my_config.forecast_scalar_estimate["pool_instruments"] = False
-print(
-    my_system.forecastScaleCap.get_forecast_scalar(
-        "EDOLLAR",
-        "ewmac32").tail(5))
-
-# or we can use the values from the book
-my_config.forecast_scalars = dict(ewmac8=5.3, ewmac32=2.65)
-my_config.use_forecast_scale_estimates = False
-fcs = ForecastScaleCap()
-my_system = System([fcs, my_rules], data, my_config)
-print(
-    my_system.forecastScaleCap.get_capped_forecast(
-        "EDOLLAR",
-        "ewmac32").tail(5))
-"""
-combine some rules
-"""
-
 from systems.forecast_combine import ForecastCombine
-
-# defaults
-combiner = ForecastCombine()
-my_system = System([fcs, my_rules, combiner], data, my_config)
-print(my_system.combForecast.get_forecast_weights("EDOLLAR").tail(5))
-print(my_system.combForecast.get_forecast_diversification_multiplier("EDOLLAR").tail(5))
-
-# estimates:
 from systems.accounts.accounts_stage import Account
 from systems.futures.rawdata import RawData
 from systems.positionsizing import PositionSizing
+from systems.portfolio import Portfolios
 
+data = csvFuturesSimData()
+
+ewmac_4 = TradingRule((ewmac, [], dict(Lfast=4, Lslow=16)))
+ewmac_8 = TradingRule(
+    dict(
+        function=ewmac,
+        other_args=dict(
+            Lfast=8,
+            Lslow=32)))
+ewmac_16 = TradingRule(
+    dict(
+        function=ewmac,
+        other_args=dict(
+            Lfast=16,
+            Lslow=64)))
+my_rules = Rules(dict(ewmac4=ewmac_4, ewmac8=ewmac_8, ewmac16=ewmac_16))
+# my_rules = Rules(dict(ewmac8=ewmac_8, ewmac16=ewmac_16))
+
+my_config = Config()
+empty_rules = Rules()
+my_config.trading_rules = dict(ewmac4=ewmac_4, ewmac8=ewmac_8, ewmac16=ewmac_16)
+# my_config.trading_rules = dict(ewmac8=ewmac_8, ewmac16=ewmac_16)
+
+# we can estimate these ourselves
+my_config.instruments = ['159949', '510050', '510330', '501077', '161912', '501046',
+                         '163417', '161005', '513050', '513100', '160416']
+my_config.use_forecast_scale_estimates = True
+my_config.start_date = '2020-01-01'
+
+# my_config.forecast_scalars = dict(ewmac4=7.5, ewmac8=5.3, ewmac16=3.75)
+# my_config.use_forecast_scale_estimates = False
+
+fcs = ForecastScaleCap()
+
+"""
+combine some rules
+"""
+# estimates:
 my_account = Account()
 combiner = ForecastCombine()
 raw_data = RawData()
 position_size = PositionSizing()
 
-
-my_config.forecast_weight_estimate = dict(method="one_period")
+my_config.forecast_weight_estimate = dict(method="shrinkage")
 my_config.use_forecast_weight_estimates = True
 my_config.use_forecast_div_mult_estimates = True
 
-my_system = System([my_account, fcs, my_rules, combiner, raw_data, position_size], data, my_config)
-
-# this is a bit slow, better to know what's going on
-my_system.set_logging_level("on")
-
-print(my_system.combForecast.get_forecast_weights("US10").tail(5))
-print(my_system.combForecast.get_forecast_diversification_multiplier("US10").tail(5))
-
-# fixed:
-my_config.forecast_weights = dict(ewmac8=0.5, ewmac32=0.5)
-my_config.forecast_div_multiplier = 1.1
-my_config.use_forecast_weight_estimates = False
-my_config.use_forecast_div_mult_estimates = False
-
-combiner = ForecastCombine()
-my_system = System(
-    [fcs, empty_rules, combiner, raw_data, position_size], data, my_config
-)  # no need for accounts if no estimation done
-my_system.combForecast.get_combined_forecast("EDOLLAR").tail(5)
-
 # size positions
-
 possizer = PositionSizing()
-my_config.percentage_vol_target = 25
-my_config.notional_trading_capital = 500000
-my_config.base_currency = "GBP"
-
-my_system = System([fcs, my_rules, combiner, possizer, raw_data], data, my_config)
-
-print(my_system.positionSize.get_price_volatility("EDOLLAR").tail(5))
-print(my_system.positionSize.get_block_value("EDOLLAR").tail(5))
-print(my_system.positionSize.get_underlying_price("EDOLLAR"))
-print(my_system.positionSize.get_instrument_value_vol("EDOLLAR").tail(5))
-print(my_system.positionSize.get_volatility_scalar("EDOLLAR").tail(5))
-print(my_system.positionSize.get_vol_target_dict())
-print(my_system.positionSize.get_subsystem_position("EDOLLAR").tail(5))
+my_config.percentage_vol_target = 20
+my_config.notional_trading_capital = 300000
+my_config.base_currency = "USD"
 
 # portfolio - estimated
-from systems.portfolio import Portfolios
-
 portfolio = Portfolios()
-
 my_config.use_instrument_weight_estimates = True
 my_config.use_instrument_div_mult_estimates = True
 my_config.instrument_weight_estimate = dict(
     method="shrinkage", date_method="in_sample")
 
-my_system = System(
-    [my_account, fcs, my_rules, combiner, possizer, portfolio, raw_data], data, my_config
-)
-
-my_system.set_logging_level("on")
-
-print(my_system.portfolio.get_instrument_weights().tail(5))
-print(my_system.portfolio.get_instrument_diversification_multiplier().tail(5))
 
 # or fixed
-portfolio = Portfolios()
-my_config.use_instrument_weight_estimates = False
-my_config.use_instrument_div_mult_estimates = False
-my_config.instrument_weights = dict(US10=0.1, EDOLLAR=0.4, CORN=0.3, SP500=0.2)
-my_config.instrument_div_multiplier = 1.5
-
-my_system = System([fcs, my_rules, combiner, possizer,
-                    portfolio, raw_data], data, my_config)
-
-print(my_system.portfolio.get_notional_position("EDOLLAR").tail(5))
+# portfolio = Portfolios()
+# my_config.use_instrument_weight_estimates = False
+# my_config.use_instrument_div_mult_estimates = False
+# my_config.instrument_weights = dict(US10=0.1, EDOLLAR=0.4, CORN=0.3, SP500=0.2)
+# my_config.instrument_div_multiplier = 1.5
+#
+# my_system = System([fcs, my_rules, combiner, possizer,
+#                     portfolio, raw_data], data, my_config)
+#
 """
 Have we made some dosh?
 """
-
+print(my_config.start_date)
+exit()
 my_system = System(
     [fcs, my_rules, combiner, possizer, portfolio, my_account, raw_data], data, my_config
 )
+my_system.set_logging_level("on")
+
+
 profits = my_system.accounts.portfolio()
-print(profits.percent.stats())
+# print(profits.percent.stats())
+profits.curve().plot()
+from matplotlib.pyplot import show
+show()
 
 # have costs data now
 print(profits.gross.percent.stats())
 print(profits.net.percent.stats())
-"""
-Another approach is to create a config object
-"""
-my_config = Config(
-    dict(
-        trading_rules=dict(ewmac8=ewmac_8, ewmac32=ewmac_32),
-        instrument_weights=dict(US10=0.1, EDOLLAR=0.4, CORN=0.3, SP500=0.2),
-        instrument_div_multiplier=1.5,
-        forecast_scalars=dict(ewmac8=5.3, ewmac32=2.65),
-        forecast_weights=dict(ewmac8=0.5, ewmac32=0.5),
-        forecast_div_multiplier=1.1,
-        percentage_vol_target=25.00,
-        notional_trading_capital=500000,
-        base_currency="GBP",
-    )
-)
-print(my_config)
-my_system = System(
-    [
-        Account(),
-        Portfolios(),
-        PositionSizing(),
-        ForecastCombine(),
-        ForecastScaleCap(),
-        Rules(),
-        RawData()
-    ],
-    data,
-    my_config,
-)
-print(my_system.portfolio.get_notional_position("EDOLLAR").tail(5))
-"""
-... or to import one
-"""
-my_config = Config("systems.provided.example.simplesystemconfig.yaml")
-print(my_config)
-my_system = System(
-    [
-        Account(),
-        Portfolios(),
-        PositionSizing(),
-        ForecastCombine(),
-        ForecastScaleCap(),
-        Rules(),
-        RawData()
-    ],
-    data,
-    my_config,
-)
-print(my_system.rules.get_raw_forecast("EDOLLAR", "ewmac32").tail(5))
-print(my_system.rules.get_raw_forecast("EDOLLAR", "ewmac8").tail(5))
-print(
-    my_system.forecastScaleCap.get_capped_forecast(
-        "EDOLLAR",
-        "ewmac32").tail(5))
-print(my_system.forecastScaleCap.get_forecast_scalar("EDOLLAR", "ewmac32"))
-print(my_system.combForecast.get_combined_forecast("EDOLLAR").tail(5))
-print(my_system.combForecast.get_forecast_weights("EDOLLAR").tail(5))
 
-print(my_system.positionSize.get_subsystem_position("EDOLLAR").tail(5))
+print(my_system.positionSize.get_price_volatility("159949").tail(5))
+print(my_system.positionSize.get_block_value("159949").tail(5))
+print(my_system.positionSize.get_underlying_price("159949"))
+print(my_system.positionSize.get_instrument_value_vol("159949").tail(5))
+print(my_system.positionSize.get_volatility_scalar("159949").tail(5))
+print(my_system.positionSize.get_vol_target_dict())
+print(my_system.positionSize.get_subsystem_position("159949").tail(5))
 
-print(my_system.portfolio.get_notional_position("EDOLLAR").tail(5))
+print(my_system.portfolio.get_instrument_weights().tail(5))
+print(my_system.portfolio.get_instrument_diversification_multiplier().tail(5))
+for code in my_config.instruments:
+    print(code)
+    print(my_system.positionSize.get_subsystem_position(code).tail(5)) # 子系统仓位
+    print(my_system.portfolio.get_notional_position(code).tail(5)) # 最终系统仓位
